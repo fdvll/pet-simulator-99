a0f9e446ae5a137801505644bc2532f3f4a6f242
Ryan <73210077+fdvll@users.noreply.github.com>
2024-02-17 11:03:19 -0800

    Update autoChest.lua

--- autoChest.lua
+++ autoChest.lua
@@ -1,20 +1,8 @@
-getgenv().autoChest = true
-
-getgenv().autoChestConfig = {
-    START_DELAY = 1, -- delay before starting
-    SERVER_HOP = true, -- server hop after breaking all chests
-    SERVER_HOP_DELAY = 1, -- delay in seconds before server hopping (set to 0 for no delay)
-    CHEST_BREAK_DELAY = 2, -- delay before breaking next chest
-    TIMER_SEARCH_DELAY = 0 -- if you are crashing or lagging, increase this value, otherwise leave it as is
-}
-
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/autoChest.lua"))()
-
 local BigChests = {
     [1] = "Beach",
     [2] = "Underworld",
     [3] = "No Path Forest",
-    -- [4] = "Heaven Gates"
+    [4] = "Heaven Gates"
 }
 
 repeat
