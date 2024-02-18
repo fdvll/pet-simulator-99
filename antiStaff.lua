dd09498a9cb60b79ad483c358fca6d367ba93654
Ryan <73210077+fdvll@users.noreply.github.com>
2024-02-17 22:24:34 -0800

    Create antiStaff.lua

---
+++ antiStaff.lua
@@ -0,0 +1,34 @@
+local WAITING = false
+
+local function serverhop(player)
+    local timeToWait = Random.new():NextInteger(300, 600)
+    print("[SAFETY] BIG Games staff (" .. player.Name ..  ") is in server! Waiting for " .. tostring(timeToWait) .. " seconds before server hopping...")
+    task.wait(timeToWait)
+
+    local success, _ = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fdvll/pet-simulator-99/main/serverhop.lua"))()
+    end)
+
+    if not success then
+        game.Players.LocalPlayer:Kick("[SAFETY] A BIG Games staff member joined and script was unable to server hop")
+    end
+end
+
+for _, player in pairs(game.Players:GetPlayers()) do
+    if player:IsInGroup(5060810) then
+        WAITING = true
+        serverhop(player)
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player:IsInGroup(5060810) and not WAITING then
+        getgenv().autoBalloon = false
+        getgenv().autoChest = false
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+        local _, zoneData = require(game:GetService("ReplicatedStorage").Library.Util.ZonesUtil).GetZoneFromNumber(Random.new():NextInteger(40, 90))
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map[tostring(zoneData["_script"])].PERSISTENT.Teleport.CFrame
+
+        serverhop(player)
+    end
+end)
