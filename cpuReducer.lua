0a2ef046c7ef45cf3f3ccb96dbc51e94c339d028
Ryan <73210077+fdvll@users.noreply.github.com>
2024-02-20 23:04:08 -0800

    Create cpuReducer.lua

---
+++ cpuReducer.lua
@@ -0,0 +1,60 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+
+local Workspace = game:GetService("Workspace")
+local Terrain = Workspace:WaitForChild("Terrain")
+Terrain.WaterReflectance = 0
+Terrain.WaterTransparency = 1
+Terrain.WaterWaveSize = 0
+Terrain.WaterWaveSpeed = 0
+
+local Lighting = game:GetService("Lighting")
+Lighting.Brightness = 0
+Lighting.GlobalShadows = false
+Lighting.FogEnd = 9e100
+Lighting.FogStart = 0
+
+sethiddenproperty(Lighting, "Technology", 2)
+sethiddenproperty(Terrain, "Decoration", false)
+
+local function clearTextures(v)
+    if v:IsA("BasePart") and not v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif (v:IsA("Decal") or v:IsA("Texture")) then
+        v.Transparency = 1
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    elseif v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+        v.TextureID = 10385902758728957
+    elseif v:IsA("SpecialMesh")  then
+        v.TextureId = 0
+    elseif v:IsA("ShirtGraphic") then
+        v.Graphic = 1
+    elseif (v:IsA("Shirt") or v:IsA("Pants")) then
+        v[v.ClassName .. "Template"] = 1
+    elseif v.Name == "Foilage" and v:IsA("Folder") then
+        v:Destroy()
+    elseif string.find(v.Name, "Tree") or string.find(v.Name, "Water") or string.find(v.Name, "Bush") or string.find(v.Name, "grass") then
+        task.wait()
+        v:Destroy()
+    end
+end
+
+game:GetService("Lighting"):ClearAllChildren()
+
+for _, v in pairs(Workspace:GetDescendants()) do
+    clearTextures(v)
+end
+
+Workspace.DescendantAdded:Connect(function(v)
+    clearTextures(v)
+end)
