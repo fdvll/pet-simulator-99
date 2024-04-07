a3b0f4aa111fdf9e819b63e6cf15b8d273a4b730
Ryan <73210077+fdvll@users.noreply.github.com>
2024-04-06 19:22:16 -0700

    Update waitForGameLoad.lua

--- waitForGameLoad.lua
+++ waitForGameLoad.lua
@@ -10,22 +10,12 @@ repeat
     task.wait(1)
 until game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
 
-
-if game.PlaceId == 8737899170 then
-    repeat
-        task.wait(1)
-    until #game:GetService("Workspace").Map:GetChildren() == 100
-elseif game.PlaceId == 16498369169 then
-    repeat
-        task.wait(1)
-    until #game:GetService("Workspace").Map2:GetChildren() == 51
-end
-
+repeat
+    task.wait(1)
+until game:GetService("Workspace"):FindFirstChild("Map") or game:GetService("Workspace"):FindFirstChild("Map2")
 
 repeat
     task.wait(1)
 until game:GetService("Workspace").__THINGS and game:GetService("Workspace").__DEBRIS
-
-task.wait(2)
 
 print("[CLIENT] Loaded Game")
